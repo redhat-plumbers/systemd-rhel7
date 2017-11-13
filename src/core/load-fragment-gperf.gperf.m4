909d27fffffb28f4da7752be25e5c47990eba641
Lennart Poettering <lennart@poettering.net>
2017-11-13 17:14:07 +0100

    core: add a new unit file setting CollectMode= for tweaking the GC logic
    Right now, the option only takes one of two possible values "inactive"
    or "inactive-or-failed", the former being the default, and exposing same
    behaviour as the status quo ante. If set to "inactive-or-failed" units
    may be collected by the GC logic when in the "failed" state too.
    
    This logic should be a nicer alternative to using the "-" modifier for
    ExecStart= and friends, as the exit data is collected and logged about
    and only removed when the GC comes along. This should be useful in
    particular for per-connection socket-activated services, as well as
    "systemd-run" command lines that shall leave no artifacts in the
    system.
    
    I was thinking about whether to expose this as a boolean, but opted for
    an enum instead, as I have the suspicion other tweaks like this might be
    a added later on, in which case we extend this setting instead of having
    to add yet another one.
    
    Also, let's add some documentation for the GC logic.
    
    (cherry-picked from commit 5afe510c89f26b0e721b276a0e78af914b47f0b0)
    
    Resolves: #1817576

--- src/core/load-fragment-gperf.gperf.m4
+++ src/core/load-fragment-gperf.gperf.m4
@@ -200,6 +200,7 @@ Unit.AssertCapability,           config_parse_unit_condition_string, CONDITION_C
 Unit.AssertHost,                 config_parse_unit_condition_string, CONDITION_HOST,                offsetof(Unit, asserts)
 Unit.AssertACPower,              config_parse_unit_condition_string, CONDITION_AC_POWER,            offsetof(Unit, asserts)
 Unit.AssertNull,                 config_parse_unit_condition_null,   0,                             offsetof(Unit, asserts)
+Unit.CollectMode,                config_parse_collect_mode,          0,                             offsetof(Unit, collect_mode)
 m4_dnl
 Service.PIDFile,                 config_parse_unit_path_printf,      0,                             offsetof(Service, pid_file)
 Service.ExecStartPre,            config_parse_exec,                  SERVICE_EXEC_START_PRE,        offsetof(Service, exec_command)
--- src/core/load-fragment.h
+++ src/core/load-fragment.h
@@ -110,6 +110,7 @@ int config_parse_cpu_quota(const char *unit, const char *filename, unsigned line
 int config_parse_protect_home(const char* unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 int config_parse_protect_system(const char* unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 int config_parse_bus_name(const char* unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
+int config_parse_collect_mode(const char *unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 
 /* gperf prototypes */
 const struct ConfigPerfItem* load_fragment_gperf_lookup(const char *key, GPERF_LEN_TYPE length);
