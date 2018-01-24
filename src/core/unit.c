94355e0146245c14012ac177461110d0a0c65f10
Lennart Poettering <lennart@poettering.net>
2018-01-24 19:54:26 +0100

    core: add a new unit_needs_console() call
    This call determines whether a specific unit currently needs access to
    the console. It's a fancy wrapper around
    exec_context_may_touch_console() ultimately, however for service units
    we'll explicitly exclude the SERVICE_EXITED state from when we report
    true.
    
    (cherry picked from commit bb2c7685454842549bc1fe47adc35cbca2a84190)
    
    Related: #1524359

--- src/core/unit.c
+++ src/core/unit.c
@@ -3699,6 +3699,28 @@ pid_t unit_main_pid(Unit *u) {
         return 0;
 }
 
+bool unit_needs_console(Unit *u) {
+        ExecContext *ec;
+        UnitActiveState state;
+
+        assert(u);
+
+        state = unit_active_state(u);
+
+        if (UNIT_IS_INACTIVE_OR_FAILED(state))
+                return false;
+
+        if (UNIT_VTABLE(u)->needs_console)
+                return UNIT_VTABLE(u)->needs_console(u);
+
+        /* If this unit type doesn't implement this call, let's use a generic fallback implementation: */
+        ec = unit_get_exec_context(u);
+        if (!ec)
+                return false;
+
+        return exec_context_may_touch_console(ec);
+}
+
 static const char* const unit_active_state_table[_UNIT_ACTIVE_STATE_MAX] = {
         [UNIT_ACTIVE] = "active",
         [UNIT_RELOADING] = "reloading",
