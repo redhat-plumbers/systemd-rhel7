8eb5f062e048ec9ce6b60dce0386b491e61950a9
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-02-13 14:37:11 +0100

    pid1: fix collection of cycles of units which reference one another
    A .socket will reference a .service unit, by registering a UnitRef with the
    .service unit. If this .service unit has the .socket unit listed in Wants or
    Sockets or such, a cycle will be created. We would not free this cycle
    properly, because we treated any unit with non-empty refs as uncollectable. To
    solve this issue, treats refs with UnitRef in u->refs_by_target similarly to
    the refs in u->dependencies, and check if the "other" unit is known to be
    needed. If it is not needed, do not treat the reference from it as preventing
    the unit we are looking at from being freed.
    
    (cherry picked from commit 2641f02e23ac7d5385db7f932aff221a063f245e)
    (cherry picked from commit 703cc4991049cdf3ad3506e432cda982b3b3b007)
    
    Resolves: #1729228

--- src/core/unit.c
+++ src/core/unit.c
@@ -288,7 +288,11 @@ bool unit_may_gc(Unit *u) {
 
         /* Checks whether the unit is ready to be unloaded for garbage collection.
          * Returns true when the unit may be collected, and false if there's some
-         * reason to keep it loaded. */
+         * reason to keep it loaded.
+         *
+         * References from other units are *not* checked here. Instead, this is done
+         * in unit_gc_sweep(), but using markers to properly collect dependency loops.
+         */
 
         if (u->job)
                 return false;
@@ -313,9 +317,6 @@ bool unit_may_gc(Unit *u) {
                 return false;
 
         if (u->no_gc)
-                return false;
-
-        if (u->refs_by_target)
                 return false;
 
         if (UNIT_VTABLE(u)->may_gc && !UNIT_VTABLE(u)->may_gc(u))
