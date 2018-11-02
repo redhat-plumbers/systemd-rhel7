0a17ed55b7440a705fe8c65f9ce8d229a0647753
Jan Synacek <jsynacek@redhat.com>
2018-11-02 08:40:40 +0100

    core: when deserializing state always use read_line(…, LONG_LINE_MAX, …)
    This should be much better than fgets(), as we can read substantially
    longer lines and overly long lines result in proper errors.
    
    Fixes a vulnerability discovered by Jann Horn at Google.
    
    (cherry picked from commit 8948b3415d762245ebf5e19d80b97d4d8cc208c1)
    
    Resolves: CVE-2018-15686
    
    [jsynacek] There were more commits in the pull request of which the cherry
    picked commit was a part of, see
    https://github.com/systemd/systemd/pull/10519/commits.
    I decided not to backport any of the remaining ones, because they were
    mostly irrelevant to the actual fix.
    
    (cherry picked from commit c3b8b320113f898377b7a5dc68275110d51ffca8)
    Resolves: #1786126

--- src/core/manager.c
+++ src/core/manager.c
@@ -37,6 +37,7 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <sys/timerfd.h>
+#include <fileio.h>
 
 #ifdef HAVE_AUDIT
 #include <libaudit.h>
@@ -2519,21 +2520,21 @@ int manager_deserialize(Manager *m, FILE *f, FDSet *fds) {
         m->n_reloading ++;
 
         for (;;) {
-                char line[LINE_MAX], *l;
+                _cleanup_free_ char *line = NULL;
+                char *l;
 
-                if (!fgets(line, sizeof(line), f)) {
-                        if (feof(f))
-                                r = 0;
-                        else
-                                r = -errno;
 
+                r = read_line(f, LONG_LINE_MAX, &line);
+                if (r < 0) {
+                        log_error("Failed to read serialization line: %m");
                         goto finish;
                 }
+                if (r == 0)
+                        break;
 
-                char_array_0(line);
                 l = strstrip(line);
 
-                if (l[0] == 0)
+                 if (isempty(l)) /* end marker */
                         break;
 
                 if (startswith(l, "current-job-id=")) {
@@ -2668,22 +2669,19 @@ int manager_deserialize(Manager *m, FILE *f, FDSet *fds) {
         }
 
         for (;;) {
+                _cleanup_free_ char *line = NULL;
                 Unit *u;
-                char name[UNIT_NAME_MAX+2];
 
                 /* Start marker */
-                if (!fgets(name, sizeof(name), f)) {
-                        if (feof(f))
-                                r = 0;
-                        else
-                                r = -errno;
-
+                r = read_line(f, LONG_LINE_MAX, &line);
+                if (r < 0) {
+                        log_error("Failed to read serialization line: %m");
                         goto finish;
                 }
+                if (r == 0)
+                        break;
 
-                char_array_0(name);
-
-                r = manager_load_unit(m, strstrip(name), NULL, NULL, &u);
+                r = manager_load_unit(m, strstrip(line), NULL, NULL, &u);
                 if (r < 0)
                         goto finish;
 
