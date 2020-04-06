systemd/systemd

sd-journal: close journal files that were deleted by journald before we've setup inotify watch
Fixes #14695

src/journal/journal-file.h
@@ -145,6 +145,7 @@ int journal_file_open(
 int journal_file_set_offline(JournalFile *f, bool wait);
 bool journal_file_is_offlining(JournalFile *f);
 JournalFile* journal_file_close(JournalFile *j);
+int journal_file_fstat(JournalFile *f);
 DEFINE_TRIVIAL_CLEANUP_FUNC(JournalFile*, journal_file_close);
 
 int journal_file_open_reliably(

redhat-plumbers/systemd-rhel7

(cherry-picked from commit 28ca867abdb20d0e4ac1901e2ed669cdb41ea3f6)
Related: #1820073

src/journal/journal-file.h
@@ -139,6 +139,7 @@ int journal_file_open(
 
 int journal_file_set_offline(JournalFile *f);
 void journal_file_close(JournalFile *j);
+int journal_file_fstat(JournalFile *j);
 
 int journal_file_open_reliably(
                 const char *fname,
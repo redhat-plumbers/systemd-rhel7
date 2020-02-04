6a6c61a3e47e8d1a0f5348a5503eae6a8fbdf19f
Michal Sekletár <msekleta@redhat.com>
2020-02-04 14:23:14 +0100

    sd-journal: close journal files that were deleted by journald before we've setup inotify watch
    (cherry-picked from commit 28ca867abdb20d0e4ac1901e2ed669cdb41ea3f6)
    
    Fixes #14695
    
    Related: #1820073

--- src/journal/journal-file.h
+++ src/journal/journal-file.h
@@ -139,6 +139,7 @@ int journal_file_open(
 
 int journal_file_set_offline(JournalFile *f);
 void journal_file_close(JournalFile *j);
+int journal_file_fstat(JournalFile *j);
 
 int journal_file_open_reliably(
                 const char *fname,
